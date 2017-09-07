b3d2548a2ed2a79fe7414d1c0af3e8a840f2fc5e
Anson <ansonchan.dudu@gmail.com>
2017-09-07 18:09:59 +0800

    add bmob

--- js/main.js
+++ js/main.js
@@ -12,29 +12,33 @@ $("input").bind('focusout', function(){
 })
 
 
-
 $('.button').click(function(){
-var a = $('#cha_1')[0].value;
-var b = $('#cha_2')[0].value;
-var c = $('#cha_3')[0].value;
+	var a = $('#cha_1')[0].value;
+	var b = $('#cha_2')[0].value;
+	var c = $('#cha_3')[0].value;
 
-var characterJson = {
-	'"1"': "" + a + "",
-	'"2"': "" + b + "",
-	'"3"': "" + c + ""
-}
+// var characterJson = {
+// 	'"1"': "" + a + "",
+// 	'"2"': "" + b + "",
+// 	'"3"': "" + c + ""
+// }
 
-$.ajax({
-	type:'POST',
-	data:JSON.stringify(characterJson),
-	contentType :'application/json',
-	dataType:'json',
-	url :'http://localhost:3000/posts',
-	success :function(data) {
-		alert("OK");
-		},
-	error :function(e) {
-		alert("error");
-		}
-	});
+// 初始化Bomb
+	Bmob.initialize("0db54364a8bec5f18b2b462e547d3e07", "9b1fc430205d355e5658db76c8f2a66e");
+	var GameScore = Bmob.Object.extend("GameScore");
+	var gameScore = new GameScore();
+	gameScore.set("character_1", a);
+	gameScore.set("character_2", b);
+	gameScore.set("character_3", c);
+	gameScore.save(null, {
+		  success: function(gameScore) {
+		    var dataId = gameScore.id;
+		    window.location.href="index.html";
+		    console.log(dataId);
+		  },
+		  error: function(gameScore, error) {
+		    alert("fail");
+		  }
+	  });
+
 })
