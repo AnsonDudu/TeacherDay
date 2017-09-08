a6c30bf3a0ffc4fa86dd82cb57ebf9d632d78409
Anson <ansonchan.dudu@gmail.com>
2017-09-08 15:37:44 +0800

    change mobile event

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var base = new Base64();
 
 // 绑定input事件
 
-$("input").bind('focusout', function(){
+var event = function(){
 	if($('#cha_1')[0].value && $('#cha_2')[0].value && $('#cha_3')[0].value&& $('#name')[0].value !== ""){
 		$('#button1').removeClass('unable');
 		$('#button1').addClass('able');
@@ -13,7 +13,17 @@ $("input").bind('focusout', function(){
 		$('#button1').addClass('unable');
 		$('#button1').removeClass('able');
 	}
-})
+}
+
+var inputs = document.getElementsByTagName('input');
+for(i = 0; i<inputs.length; i++){
+	inputs[i].addEventListener('touchstart',event);
+	inputs[i].addEventListener('touchend',event);
+	inputs[i].addEventListener('onfocusout',event);
+}
+
+
+
 
 
 $('.button').click(function(){
@@ -29,24 +39,6 @@ $('.button').click(function(){
 
 	var url = "page3.html?"+a_+"&"+b_+"&"+c_+"&"+d_;
 	location.href = url;
-
-// // 初始化Bomb
-// 	Bmob.initialize("0db54364a8bec5f18b2b462e547d3e07", "9b1fc430205d355e5658db76c8f2a66e");
-// 	var GameScore = Bmob.Object.extend("GameScore");
-// 	var gameScore = new GameScore();
-// 	gameScore.set("character_1", a);
-// 	gameScore.set("character_2", b);
-// 	gameScore.set("character_3", c);
-// 	gameScore.save(null, {
-// 		  success: function(gameScore) {
-// 		    LocalData = gameScore.id;
-// 		    window.location.href="index.html";
-// 		    console.log(dataId);
-// 		  },
-// 		  error: function(gameScore, error) {
-// 		    alert("fail");
-// 		  }
-// 	  });
 
 })
 
